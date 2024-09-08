85ebb7fb11f211079ddf1b06b011589b73907b42
yudha <yamashitataiki123@gmail.com>
2024-09-08 20:06:34 +0700

    Escape the Jar minigame

---
+++ Assets/Scripts/Components/ExtraComponents/LauncherComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Components.ExtraComponents
+{
+    public class LauncherComponent : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private Vector2 launchPower;
+
+        public UnityEvent onLaunchedEvent;
+        
+        public void Launch()
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            rb.mass = 1;
+            rb.gravityScale = 0;
+            rb.AddForce(launchPower);
+            onLaunchedEvent.Invoke();
+        }
+    }
+}
