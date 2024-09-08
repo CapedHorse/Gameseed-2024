ac89d03ff1deae9ea256405d75218b24b2840b23
yudha <yamashitataiki123@gmail.com>
2024-09-08 13:34:37 +0700

    Added tester system

---
+++ Assets/Scripts/Test/GameAndTutorSessionTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using Level;
+using Tutorial;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Test
+{
+    public class GameAndTutorSessionTest : MonoBehaviour
+    {
+        private GameSession testedGameSession;
+        private TutorialSession testedGamTutorialSession;
+        [SerializeField] private float testingDelayTime;
+
+        private IEnumerator Start()
+        {
+            testedGameSession = FindObjectOfType<GameSession>();
+            testedGamTutorialSession = FindObjectOfType<TutorialSession>();
+            
+            yield return new WaitForSeconds(testingDelayTime);
+            if (testedGameSession)
+            {
+                testedGameSession.StartGame();
+                yield return null;
+            }
+
+            if (testedGamTutorialSession)
+            {
+                testedGamTutorialSession.StartTutor();
+                yield return null;
+            }
+        }
+    }
+}
